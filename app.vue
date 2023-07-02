0965fad087e876131bc1e335617a0d0f9d10832c
ParkBeomMin <club20608@gmail.com>
2023-07-02 15:27:58 +0900

    seo

--- app.vue
+++ app.vue
@@ -19,6 +19,16 @@ useHead({
         },
     ],
 });
+useSeoMeta({
+    title: "AboutMe",
+    ogTitle: "AboutMe",
+    description:
+        "친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!",
+    ogDescription:
+        "친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!",
+    ogImage:
+        "https://about-me-tree.web.app/assets/images/sprout.3d595a3260f1a1ed70c22930ea3f2876.svg",
+});
 </script>
 
 <style>
