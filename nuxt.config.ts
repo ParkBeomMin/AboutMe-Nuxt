455afe1ff77f3f1666dceda8ff062af90a484608
bmpark <bmpark@jinhak.com>
2023-07-03 10:20:41 +0900

    sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { isProduction } from "std-env";
+import { isProduction } from 'std-env';
 export default defineNuxtConfig({
     devtools: { enabled: true },
     runtimeConfig: {
@@ -9,6 +9,13 @@ export default defineNuxtConfig({
         },
     },
     nitro: {
-        preset: "firebase",
+        preset: 'firebase',
+    },
+    modules: ['@nuxtjs/sitemap'],
+    sitemap: {
+        hostname: 'https://aboutme2.web.app',
+        gzip: true,
+        exclude: [],
+        routes: ['/', '/me/*', '/about/*'],
     },
 });
