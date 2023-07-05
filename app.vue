f453b9d7390cc34ab4f474172b152f28a15d2230
bmpark <bmpark@jinhak.com>
2023-07-05 11:51:01 +0900

    카카오광고는 팝업으로 띄우면 안됨

--- app.vue
+++ app.vue
@@ -12,35 +12,31 @@ const { passwordLayerData } = usePassWordLayer();
 const { moneyLayerData } = useMoneyLayer();
 const { adLayerData, setIsShowAdLayer } = useAdLayer();
 useHead({
-    title: "AboutMe",
+    title: 'AboutMe',
     meta: [
         {
-            name: "description",
-            content:
-                "친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!",
+            name: 'description',
+            content: '친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!',
         },
     ],
 });
 useSeoMeta({
-    title: "AboutMe",
-    ogTitle: "AboutMe",
-    description:
-        "친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!",
-    ogDescription:
-        "친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!",
-    ogImage:
-        "https://about-me-tree.web.app/assets/images/sprout.3d595a3260f1a1ed70c22930ea3f2876.svg",
+    title: 'AboutMe',
+    ogTitle: 'AboutMe',
+    description: '친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!',
+    ogDescription: '친구들이 생각하는 나는 어떤 사람일까? 친구들의 마음이 모여 나의 새싹을 키워보세요!',
+    ogImage: 'https://about-me-tree.web.app/assets/images/sprout.3d595a3260f1a1ed70c22930ea3f2876.svg',
 });
 const route = useRoute();
 
-watch(
-    route,
-    (value) => {
-        const isShow = !!Math.round(Math.random());
-        setIsShowAdLayer({ isShow });
-    },
-    { deep: true, immediate: true }
-);
+// watch(
+//     route,
+//     (value) => {
+//         const isShow = !!Math.round(Math.random());
+//         setIsShowAdLayer({ isShow });
+//     },
+//     { deep: true, immediate: true }
+// );
 </script>
 
 <style>
--- pages/About/[id].vue
+++ pages/About/[id].vue
@@ -7,14 +7,10 @@
                 <SvgSend />
             </button>
         </form>
-        <span class="info"
-            >ex) 마음이 따듯해. 예뻐. 냉정해. 극I...<br />친구를 떠올렸을 때
-            생각나는 말을 적어주세요.</span
-        >
+        <span class="info">ex) 마음이 따듯해. 예뻐. 냉정해. 극I...<br />친구를 떠올렸을 때 생각나는 말을 적어주세요.</span>
         <nuxt-link class="go-new" to="/"> 새싹 만들러가기🌱</nuxt-link>
-        <a href="javascript:void(0)" class="go-my" @click="showMyAbout({ id })">
-            내 새싹 보러가기💚
-        </a>
+        <a href="javascript:void(0)" class="go-my" @click="showMyAbout({ id })"> 내 새싹 보러가기💚 </a>
+        <ins class="kakao_ad_area" style="display: none" data-ad-unit="DAN-2AsGSBANSChk42YM" data-ad-width="250" data-ad-height="250"></ins>
     </div>
 </template>
 
@@ -26,8 +22,18 @@ const submit = (e: FormDataEvent) => {
     e.preventDefault();
     sendAbout({ id: id as string });
 };
+useHead({
+    script: [
+        {
+            src: '//t1.daumcdn.net/kas/static/ba.min.js',
+            tagPosition: 'head',
+            type: 'text/javascript',
+            async: true,
+        },
+    ],
+});
 </script>
 
 <style scoped>
-@import "~/assets/css/About.css";
+@import '~/assets/css/About.css';
 </style>
