66ef936d6ccc0500b303ac96b0157e55992cef42
bmpark <bmpark@jinhak.com>
2023-07-06 10:58:18 +0900

    tree 오류 수정

--- server/api/aboutMe.get.ts
+++ server/api/aboutMe.get.ts
@@ -1,26 +1,26 @@
-import { firestore } from "../utils/firebase";
+import { firestore } from '../utils/firebase';
 
 export default defineEventHandler(async (event) => {
     const { id } = getQuery(event);
 
     let aboutList = [];
-    let color = "rgb(0,0,0)";
-    let groundColor = "rgb(0,0,0)";
-    let num = "1";
-    let type = "sprout";
+    let color = 'rgb(0,0,0)';
+    let groundColor = 'rgb(0,0,0)';
+    let num = '1';
+    let type = 'sprout';
 
-    if (id == "new") {
-        const ref = await firestore.collection("abouts");
+    if (id == 'new') {
+        const ref = await firestore.collection('abouts');
         const snapshot = await ref.get();
         aboutList = snapshot.docs.map((doc) => doc.data());
     } else {
-        const ref = await firestore.collection("users").doc(id as string);
+        const ref = await firestore.collection('users').doc(id as string);
         const doc = await ref.get();
 
         const aboutCol = await firestore
-            .collection("users")
+            .collection('users')
             .doc(id as string)
-            .collection("about");
+            .collection('about');
         const snapshot = await aboutCol.get();
         aboutList = snapshot.docs.map((doc) => doc.data());
         const ds = doc.data() as any;
@@ -29,9 +29,10 @@ export default defineEventHandler(async (event) => {
         num = ds.num;
     }
     if (aboutList.length > 10) {
-        type = "growing";
-    } else if (aboutList.length > 50) {
-        type = "tree";
+        type = 'growing';
+    }
+    if (aboutList.length > 50) {
+        type = 'tree';
     }
     return {
         color,
